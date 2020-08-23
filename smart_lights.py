27ddbb61d2edc4c46784b5cf207e3ba0e61a32ba
Sameer <36122808+sameermohanty02@users.noreply.github.com>
2020-08-23 23:32:49 +0530

    Update smart_lights.py

--- smart_lights.py
+++ smart_lights.py
@@ -2,7 +2,7 @@ from phue import Bridge
 from ip_address import bridge_ip_address
 import time
 
-# function to connect to the flash light
+# function to connect to the smart light
 def access_lights(bridge_ip_address):
     b = Bridge(bridge_ip_address)
     light_names_list = b.get_light_objects('name')
